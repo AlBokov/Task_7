f96490cfdc3e3010e8aa304d372c460e5e079ac0
Alexander Bokov <alexander.bokov30@yandex.ru>
2024-03-20 13:10:17 +0300

    Task_2 параметризированные на основе CSV-файлов

--- src/test/java/ru/netology/services/Task_7/services/serviceExpensesTest.java
+++ src/test/java/ru/netology/services/Task_7/services/serviceExpensesTest.java
@@ -2,15 +2,13 @@ package ru.netology.services.Task_7.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class serviceExpensesTest {
 
     @ParameterizedTest
-    @CsvSource({
-            "100000,60000,150000,2",
-            "10000,3000,20000,3"
-    })
+    @CsvFileSource(files="src/test/resources/bonus.csv")
     public void testNumberMonthsVacation(int income, int expenses, int threshold, int monthVacation) {
         serviceExpenses service = new serviceExpenses();
 

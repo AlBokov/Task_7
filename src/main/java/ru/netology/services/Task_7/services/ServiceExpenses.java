20407e974ed8ef18d77000c08bed7582f32dd5b5
Alexander Bokov <alexander.bokov30@yandex.ru>
2024-03-31 02:34:47 +0300

    Упрощение выражения поиска в функции ServiceExpenses

--- src/main/java/ru/netology/services/Task_7/services/ServiceExpenses.java
+++ src/main/java/ru/netology/services/Task_7/services/ServiceExpenses.java
@@ -8,7 +8,7 @@ public class ServiceExpenses {
 
         for (int month = 0; month < 12; month++) {
             if (money > threshold) {
-                money = money - expenses - ((money - expenses) / 3 * 2);
+                money = (money - expenses) / 3;
                 count++;
             } else {
                 money = money + income - expenses;
